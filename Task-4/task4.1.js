3679f670721fb3c465558266c1f6c9dc6a6c6f3f
1Bitcoin <shigalkin@mail.ru>
2020-10-10 14:03:38 +0300

    task4.1 complete

---
+++ Task-4/task4.1.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+const port = 5015;
+app.listen(port);
+console.log("My server on port " + port);
+
+app.get("/me/page", function(request, response) 
+{
+    const nameString = request.query.p;
+    if (fs.existsSync(nameString)) 
+    {
+        const contentString = fs.readFileSync(nameString, "utf8");
+        response.end(contentString);
+    } 
+    else 
+    {
+        const contentString = fs.readFileSync("bad.html", "utf8");
+        response.end(contentString);
+    }
+});
+
+app.get("/calculate/max", function(request, response) 
+{
+    var answerJSON = JSON.stringify({result: "badInput"});
+
+    const a = request.query.first;
+    const b = request.query.second;
+    const c = request.query.third;
+
+    if (a == null || b == null || c == null)
+    {
+        response.end(answerJSON);
+    }
+    else
+    {   
+        const aInt = parseInt(a);
+        const bInt = parseInt(b);
+        const cInt = parseInt(c);
+
+        const maxElem = Math.max(aInt, bInt, cInt);
+
+        answerJSON = JSON.stringify({result: maxElem});
+        response.end(answerJSON);
+    }
+});
