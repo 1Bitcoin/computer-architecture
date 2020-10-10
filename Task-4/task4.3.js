0806d6a55b928c016efdb5400668760853de6236
1Bitcoin <shigalkin@mail.ru>
2020-10-10 16:25:59 +0300

    task4.3 skip && task4.4 complete

---
+++ Task-4/task4.3.js
@@ -0,0 +1,24 @@
+"use strict";
+//todo
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+const port = 5015;
+app.listen(port);
+console.log("My server on port " + port);
+
+app.get("/me/page", function(request, response) 
+{
+    const nameString = request.query.p;
+    if (fs.existsSync(nameString)) 
+    {
+        const contentString = fs.readFileSync(nameString, "utf8");
+        response.end(contentString);
+    } 
+    else 
+    {
+        const contentString = fs.readFileSync("bad.html", "utf8");
+        response.end(contentString);
+    }
+});
---
+++ Task-4/task4.4.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+const port = 5015;
+app.listen(port);
+console.log("My server on port " + port);
+
+app.get("/page", function(request, response) 
+{
+    const nameString = request.query.p;
+    if (fs.existsSync(nameString)) 
+    {
+        const contentString = fs.readFileSync(nameString, "utf8");
+        response.end(contentString);
+    } 
+    else 
+    {
+        const contentString = fs.readFileSync("bad.html", "utf8");
+        response.end(contentString);
+    }
+});
+
+app.get("/calculate/special", function(request, response) 
+{
+    var answerJSON = JSON.stringify({result: "badInput"});
+
+    const a = request.query.first;
+    const b = request.query.second;
+    const c = request.query.third;
+
+    const aInt = parseInt(a);
+    const bInt = parseInt(b);
+    const cInt = parseInt(c);
+
+    if (aInt == null || bInt == null || cInt == null)
+    {
+        response.end(answerJSON);
+    }
+    else
+    {   
+        const maxElem = Math.max(aInt, bInt, cInt);
+
+        answerJSON = JSON.stringify(getAnswer(aInt, bInt, cInt));
+        response.end(answerJSON);
+    }
+});
+
+function getAnswer(a, b, c)
+{
+    var answer = [];
+
+    for (var i = a; i <= b; i++)
+    {
+        if (i % c == 0)
+            answer.push(i);
+    }
+
+    return answer;
+}
